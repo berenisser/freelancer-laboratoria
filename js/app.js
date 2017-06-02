f4bfa1f1b2ab30f95b7bd29be04cf74fd33dde26
berenisser <berenisser@gmail.com>
2017-06-01 21:55:59 -0400

    Changes done

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@
 	var boxes = Array.from(document.getElementsByClassName("imagenes"));
 	var portfolio = document.getElementById("portfolio");
 
-	var modal, bodyModal, close, img;
+	var modal, bodyModal, close, img, titulo, tituloText, star, contenido, contenidoTexto;
 	boxes.forEach(function(box){
 		box.addEventListener("click", function(){
 			modal = document.createElement("div");
@@ -15,7 +15,23 @@
 			bodyModal.classList.add("modal-body");
 			bodyModal.innerHTML = box.innerHTML;			
 			modal.appendChild(bodyModal);
-			
+
+			titulo = document.createElement("div");
+			tituloText = document.createTextNode("PROJECT TITLE");
+			titulo.classList.add("titulo");
+			titulo.appendChild(tituloText);
+			modal.appendChild(titulo);
+
+			star = document.createElement("hr");
+			star.classList.add("estrellita");
+			titulo.appendChild(star);
+
+			contenido = document.createElement("p");
+			contenido.classList.add("contenido");
+			contenidoTexto = document.createTextNode("Use this area of the page to describe your project. The icon above is part of a free icon set by Flat Icons. On their website, you can download their free set with 16 icons, or you can purchase the entire set with 146 icons for only $12!");
+			contenido.appendChild(contenidoTexto);
+			titulo.appendChild(contenido);
+
 			close = document.createElement("div");
 			close.classList.add("close");
 			img = document.createElement("img");
